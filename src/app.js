cc5d7f3cfb7cc6419f603cea58ec12286f561871
MCGiako <giacometto.r.h2@gmail.com>
2021-08-10 09:07:26 +0000

    coderandomcardgenerator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var numero = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  var pinta = ["♦", "♥", "♠", "♣"];
+  var randnumero = numero[Math.floor(Math.random() * numero.length)];
+  var randpinta = pinta[Math.floor(Math.random() * pinta.length)];
+  console.log(randpinta, randnumero, randpinta);
+  var up = document.querySelector(".up");
+  up.innerHTML = randpinta;
+  var middle = document.querySelector(".middle");
+  middle.innerHTML = randnumero;
+  var down = document.querySelector(".down");
+  down.innerHTML = randpinta;
 };
